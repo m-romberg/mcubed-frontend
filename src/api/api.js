319f00c7cda1c69a869c9f0640f21ede2a9b67ed
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-06-17 16:30:44 -0700

    add api class to make requests

--- src/api/api.js
+++ src/api/api.js
@@ -2,3 +2,31 @@ import axios from "axios";
 
 const BASE_URL = "https://us-central1-aiot-fit-xlab.cloudfunctions.net/mcubed";
 
+/**
+ * API class
+ *
+ * Make calls to request
+ */
+class MccubedApi {
+
+  //generic request to the api
+  static async request(endpoint, data = {}, method = "get") {
+    console.debug("API Call:", endpoint, data, method);
+
+    const url = `${BASE_URL}/${endpoint}`;
+    const params = (method === "get")
+        ? data
+        : {};
+
+    try {
+      return (await axios({ url, method, data, params })).data;
+    } catch (err) {
+      console.error("API Error:", err.response);
+      let message = err.response.data.error.message;
+      throw Array.isArray(message) ? message : [message];
+    }
+  }
+
+}
+
+export default MccubedApi;
