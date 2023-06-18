c085319cbf7e611dd387e441460496721648f428
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-06-17 22:56:09 -0700

    add navigation to entire app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import MccubedApi from './api/api';
 import {useState} from "react";
 import RoutesList from './routes-nav/RoutesList';
-import ApiForms from './forms-api/ApiForms';
+import Navigation from './routes-nav/Navigation';
+// import ApiForms from './forms-api/ApiForms';
 
 /**
  * App
@@ -11,18 +12,20 @@ import ApiForms from './forms-api/ApiForms';
  */
 function App() {
 
-
+  let questions = [];
   let request;
   async function requests(data, method){
     console.log("requests", "data", data, "method", method)
     const endpoint="";
     request = await MccubedApi.request(endpoint,data,method);
+    console.log("request", request);
   }
 
 
   return (
     <div className="App">
-      <RoutesList submit={requests} />
+    <Navigation />
+      <RoutesList submit={requests} questions={questions} />
     </div>
   );
 }
